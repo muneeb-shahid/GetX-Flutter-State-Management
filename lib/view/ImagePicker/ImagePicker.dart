a22fdfdfb40631d1a2913289a81e6903769e6ed4
muneeb-shahid <muneebshahid0102@gmail.com>
2023-03-19 23:36:34 +0500

    updated commit

--- lib/view/ImagePicker/ImagePicker.dart
+++ lib/view/ImagePicker/ImagePicker.dart
@@ -1,13 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:get/get.dart';
+
+import '../../controller/ImagePickerController/ImagePickerController.dart';
 
 class ImagePicker extends StatelessWidget {
   const ImagePicker({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final ImagePickerController _Image_Picker_Controller =
+        Get.put(ImagePickerController());
     return Scaffold(
         appBar: AppBar(
           title: Text("ImagePicker"),
@@ -15,12 +21,22 @@ class ImagePicker extends StatelessWidget {
         ),
         body: Obx(() {
           return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Center(
                 child: CircleAvatar(
-                  
-                ),
-              )
+                    radius: 50,
+                    backgroundImage: _Image_Picker_Controller
+                            .imagePath.isNotEmpty
+                        ? FileImage(
+                            File(_Image_Picker_Controller.imagePath.toString()))
+                        : null),
+              ),
+              TextButton(
+                  onPressed: () {
+                    _Image_Picker_Controller.getImageFromCamera();
+                  },
+                  child: Text("Pick Image"))
             ],
           );
         }));
