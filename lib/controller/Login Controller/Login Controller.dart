7785a33a36c3fc87ac9df0375202d47b0bbecddc
muneeb-shahid <muneebshahid0102@gmail.com>
2023-03-21 23:58:08 +0500

    updated

--- lib/controller/Login Controller/Login Controller.dart
+++ lib/controller/Login Controller/Login Controller.dart
@@ -1,8 +1,21 @@
+import 'dart:html';
+
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart';
 
 class Login_Controller extends GetxController {
   final emailController = TextEditingController().obs;
   final passwordController = TextEditingController().obs;
 
+  void loginAPI() async {
+    final Response = await post(Uri.parse("https://reqres.in/api/login"),
+    body:
+    {
+      "email": '',
+     "password":'', 
+    }
+    );
+    
+  }
 }
--- lib/view/Login/Login .dart
+++ lib/view/Login/Login .dart
@@ -17,49 +17,54 @@ class LoginView extends StatelessWidget {
         title: Text("Login Screen"),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-            child: TextField(
-              controller: _login_Controller.emailController.value,
-              decoration: InputDecoration(hintText: 'Email'),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-            child: TextField(
-              controller: _login_Controller.passwordController.value,
-              decoration: InputDecoration(hintText: 'Password'),
-            ),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          GestureDetector(
-            onTap: () {},
-            child: Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20),
-              child: Container(
-                height: MediaQuery.of(context).size.height * 0.06,
-                width: MediaQuery.of(context).size.width * 0.50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Constants_Colors.green_color,
-                ),
-                child: Center(
-                    child: Text(
-                  'Login',
-                  style: TextStyle(
-                      fontFamily: 'Inter',
-                      fontSize: 18,
-                      fontWeight: FontWeight.w700,
-                      color: Constants_Colors.White_Color),
-                )),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+              child: TextField(
+                controller: _login_Controller.emailController.value,
+                decoration: InputDecoration(hintText: 'Email'),
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+              child: TextField(
+                controller: _login_Controller.passwordController.value,
+                decoration: InputDecoration(hintText: 'Password'),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            GestureDetector(
+              onTap: () {
+                _login_Controller.loginAPI();
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.06,
+                  width: MediaQuery.of(context).size.width * 0.50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Constants_Colors.green_color,
+                  ),
+                  child: Center(
+                      child: Text(
+                    'Login',
+                    style: TextStyle(
+                        fontFamily: 'Inter',
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                        color: Constants_Colors.White_Color),
+                  )),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
