5cb844c939d0236d9c2b94fce79da0704a9d9f85
muneeb-shahid <muneebshahid0102@gmail.com>
2023-03-21 23:34:29 +0500

    NEW COMMIT

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:getx/view/CounterApp/counterapp.dart';
 import 'package:getx/view/Dark%20theme%20Light%20theme/GetX%20Dark%20theme%20Light%20theme.dart';
 import 'package:getx/view/Dialog%20Alert/getX_Dialog_Alert.dart';
 import 'package:getx/view/ImagePicker/ImagePicker.dart';
+import 'package:getx/view/Login/Login%20.dart';
 import 'package:getx/view/Routes,Navigations%20and%20data%20pass/ScreenOne.dart';
 import 'package:getx/view/Routes,Navigations%20and%20data%20pass/ScreenTwo.dart';
 import 'package:getx/view/SnackBar/getX_SnackBar.dart';
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ImagePicker(),
+      home: LoginView(),
       getPages: [
         GetPage(name: '/', page: () => GetX_SnackBar()),
         GetPage(name: '/GetX_Dialog_Alert', page: () => GetX_Dialog_Alert()),
---
+++ lib/view/Login/Login .dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Screen"),
+        centerTitle: true,
+      ),
+      body: Column(
+
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
