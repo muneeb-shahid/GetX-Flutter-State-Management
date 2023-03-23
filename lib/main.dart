56b09543655ea3d82f595cdebc5b5dc4e7a0b1dd
muneeb-shahid <muneebshahid0102@gmail.com>
2023-03-23 22:33:49 +0500

    COMMIT

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // home: AppException(),
-      getPages: AppRoutes.appRoutes()
+      // getPages: AppRoutes.appRoutes()
       //   GetPage(name: '/', page: () => GetX_SnackBar()),
       //   GetPage(name: '/GetX_Dialog_Alert', page: () => GetX_Dialog_Alert()),
       //   GetPage(
